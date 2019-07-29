9ccbab4d3eb030cb72e2b29c702633d7af8a0996
Nguyenthichan <53024820+Nguyenthichan@users.noreply.github.com>
2019-07-29 10:48:53 +0700

    Add files via upload

---
+++ chan39a/chan39a/WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="chan39a.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="lbLGUESTBOOK" runat="server" ForeColor="#FF3300" Text="GUESTBOOK"></asp:Label>
+    <div>
+    
+        <asp:Label ID="lbltieude" runat="server" Text="Tiêu đề:"></asp:Label>
+        <asp:TextBox ID="txttieude" runat="server" style="margin-left: 61px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblhoten" runat="server" Text="Họ tên:"></asp:Label>
+        <asp:TextBox ID="txthoten" runat="server" style="margin-left: 63px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblemail" runat="server" Text="Email:"></asp:Label>
+        <asp:TextBox ID="txtemail" runat="server" style="margin-left: 70px"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblnoidung" runat="server" Text="Nội dung:"></asp:Label>
+        <asp:TextBox ID="txtnoidung" runat="server" style="margin-left: 47px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btngnd" runat="server" style="margin-left: 265px" Text="Gửi nội dung" Width="233px" OnClick="btngnd_Click" />
+        <br />
+        <asp:Button ID="btndscm" runat="server" Text="DANH SÁCH COMMENT" Width="772px" />
+    
+        <br />
+        <Span id="EntryComment" runat="server"/>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ chan39a/chan39a/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace chan39a
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Comment();
+        }
+        protected void btngnd_Click(object sender, EventArgs e)
+        {
+            string file = Server.MapPath(@"\") + "data.txt";
+            using (StreamWriter writer = new StreamWriter(file, true))
+            {
+                writer.WriteLine(txttieude.Text);
+                writer.WriteLine(txthoten.Text);
+                writer.WriteLine(txtemail.Text);
+                writer.WriteLine(txtnoidung.Text);
+                writer.WriteLine("#END");
+                writer.Close();
+            }
+        }
+        private void Comment(){
+            string file=Server.MapPath(@"\") + "data.txt";
+            using(StreamReader reader=new StreamReader(file))
+            {
+                string noidung=reader.ReadToEnd();
+                string [] delimiter={"#END"};
+                string[] arr=noidung.Split(delimiter,StringSplitOptions.RemoveEmptyEntries);
+                foreach(string s in arr){
+                    string Xuongdong;
+                    Xuongdong=Regex.Replace(s,@"\r\n",@"<br/>");
+                    string entry=string.Format("<tr><td colspan=\"2\"{0}></td></tr>",Xuongdong);
+                    EntryComment.InnerHtml += entry;
+
+
+
+                }
+
+            }
+        }
+    }
+}
