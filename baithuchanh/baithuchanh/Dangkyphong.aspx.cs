f19da5037811bc7a80ea09bd421b7939a26cc0f3
Nguyenthichan <53024820+Nguyenthichan@users.noreply.github.com>
2019-08-07 11:07:46 +0700

    đăng ký phòng đã hoàn thành
    chayyoo

---
+++ baithuchanh/baithuchanh/Dangkyphong.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace baithuchanh
+{
+    public partial class Dangkyphong : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btndangky_Click(object sender, EventArgs e)
+        {
+            if(Page.IsValid)//kiểm tra dữ liệu thành công
+            {
+                lbthongbao.Text = string.Format("Bạn {0} đã đăng ký thành công !",txthoten.Text);
+            }
+        }
+      
+    }
+}
